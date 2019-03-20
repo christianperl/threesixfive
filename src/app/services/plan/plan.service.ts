179a378e54d30125fa3240d5d59101302045411a
Pascal Skupa <pascal.skupa@htl.rennweg.at>
2019-03-20 14:38:36 +0100

    Plan First Design

--- src/app/services/plan/plan.service.ts
+++ src/app/services/plan/plan.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class PlanService {
   clickedDate;
-  actualView = 'monthComponent';
+  actualView = 'weekComponent';
   actualDate = new Date();
   constructor(private http: HttpClient) { }
   dayIsClicked(date) {
