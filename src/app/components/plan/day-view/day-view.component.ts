00fe40de1bd513289975562f4a3c4f95cf4c8051
Marwan Abdalla <4160@htl.rennweg,at>
2019-03-29 14:05:50 +0100

    sdffadf

--- src/app/components/plan/day-view/day-view.component.ts
+++ src/app/components/plan/day-view/day-view.component.ts
@@ -1,8 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {PlanService} from '../../../services/plan/plan.service';
 import {LumenService} from '../../../services/lumen/lumen.service';
-import data1 from '../../../services/plan/response_day';
-import {forEach} from '@angular/router/src/utils/collection';
+
 
 @Component({
   selector: 'app-day-view',
@@ -34,8 +33,8 @@ export class DayViewComponent implements OnInit {
         this.dailyMeal = result;
 
         ['breakfast', 'lunch', 'main dish', 'snack'].forEach(type => {
-          const keys = Object.keys(day);
-          const index = keys.indexOf(type);
+          const keys2 = Object.keys(day);
+          const index = keys2.indexOf(type);
           result[Object.values(day)[index]['name']] =
             [Object.values(day)[index]['ingredients'], Object.values(day)[index]['directions'], [Object.values(day)[index]['nutrition']]];
         });
